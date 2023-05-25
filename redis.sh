aca66d3daa80f772dff6f5c0743c52e9378de400
Girish <girishiva1989@gmail.com>
2023-05-25 17:19:05 +0530

    Create redis script

---
+++ redis.sh
@@ -0,0 +1,10 @@
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+yum module enable redis:remi-6.2 -y
+
+yum install redis -y
+
+## Edit the file
+
+systemctl enable redis
+systemctl start redis
