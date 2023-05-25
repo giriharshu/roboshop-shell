e65465a4f7c44372dbcb20d7ac388fa3c54929d4
Girish <girishiva1989@gmail.com>
2023-05-25 17:22:10 +0530

    Create user script

---
+++ user.sh
@@ -0,0 +1,27 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+
+npm install
+
+cp user.servie /etc/systemd/system/user.service
+
+systemctl daemon-reload
+
+systemctl enable user
+systemctl start user
+
+cp mango.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
