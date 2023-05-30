988b5601db35fdede0051c813be77b9482a1db3a
Girish <girishiva1989@gmail.com>
2023-05-30 19:47:28 +0530

    updated SED command

--- mangodb.sh
+++ mangodb.sh
@@ -6,6 +6,6 @@ systemctl enable mongod
 systemctl start mongod
 
 ## Edit the file and replace 122.0.0.1 with 0.0.0.0
-sed -i -e 's|122.0.0.1|0.0.0.0' /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
 systemctl restart mongod
