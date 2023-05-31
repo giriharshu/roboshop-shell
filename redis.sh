ef063eec99d5c0c3f873aa89e6dbc9be16fe61fb
Girish <girishiva1989@gmail.com>
2023-05-31 19:27:25 +0530

    Added sed command to update the config file

--- redis.sh
+++ redis.sh
@@ -1,10 +1,14 @@
+echo -e "\e[36m>>>>>>>>>> Install remirepo <<<<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
+echo -e "\e[36m>>>>>>>>>> Enable Redis 6.2<<<<<<<<<<<\e[0m"
 yum module enable redis:remi-6.2 -y
 
+echo -e "\e[36m>>>>>>>>>> Install Redis <<<<<<<<<<<\e[0m"
 yum install redis -y
 
-## Edit the file
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+
 
 systemctl enable redis
 systemctl start redis
