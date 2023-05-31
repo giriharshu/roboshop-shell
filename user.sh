c2230095b06300914b8375b84746dfc5cd263fb9
Girish <girishiva1989@gmail.com>
2023-05-31 21:11:53 +0530

    Updated cp commands

--- user.sh
+++ user.sh
@@ -20,7 +20,7 @@ echo -e "\e[36m>>>>>>>>>> Install NodeJS dependencies <<<<<<<<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>>>>>> Install NodeJS dependencies <<<<<<<<<<<\e[0m"
-cp /home/centos/roboshop-shell/user.servie /etc/systemd/system/user.service
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
 
 echo -e "\e[36m>>>>>>>>>> Start User Services <<<<<<<<<<<\e[0m"
 systemctl daemon-reload
