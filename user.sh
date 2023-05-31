ffd78b60faa3013e3518b30d8e111880e674abbe
Girish <girishiva1989@gmail.com>
2023-05-31 20:26:46 +0530

    Added title to script

--- user.sh
+++ user.sh
@@ -1,27 +1,37 @@
+echo -e "\e[36m>>>>>>>>>>Configuring NodeJS repos <<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
+echo -e "\e[36m>>>>>>>>>> Install NodeJS <<<<<<<<<<<\e[0m"
 yum install nodejs -y
 
+echo -e "\e[36m>>>>>>>>>> Add Application USer <<<<<<<<<<<\e[0m"
 useradd roboshop
 
+echo -e "\e[36m>>>>>>>>>> Create Application Directory <<<<<<<<<<<\e[0m"
 mkdir /app
 
+echo -e "\e[36m>>>>>>>>>> Unzip App Contents <<<<<<<<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
 unzip /tmp/user.zip
 
+echo -e "\e[36m>>>>>>>>>> Install NodeJS dependencies <<<<<<<<<<<\e[0m"
 npm install
 
+echo -e "\e[36m>>>>>>>>>> Install NodeJS dependencies <<<<<<<<<<<\e[0m"
 cp user.servie /etc/systemd/system/user.service
 
+echo -e "\e[36m>>>>>>>>>> Start User Services <<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 
 systemctl enable user
 systemctl start user
 
+echo -e "\e[36m>>>>>>>>>> Copy Mangodb repo <<<<<<<<<<<\e[0m"
 cp mango.repo /etc/yum.repos.d/mongo.repo
 
+echo -e "\e[36m>>>>>>>>>> Install Mangodb Client <<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+mongo --host mangodb-dev.gdevops89.online </app/schema/catalogue.js
 
