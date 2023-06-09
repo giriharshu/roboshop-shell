9b76385776830b4fe8eb505ad3739df15de6eedc
Girish <girishiva1989@gmail.com>
2023-06-09 16:55:07 +0530

    Add Domain Name Add comments

--- cart.sh
+++ cart.sh
@@ -1,18 +1,26 @@
+echo -e "\e[36m>>>>>>>>>>Configuring NodeJS repos <<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
+echo -e "\e[36m>>>>>>>>>>Install NodeJS <<<<<<<<<<<\e[0m"
 yum install nodejs -y
 
+echo -e "\e[36m>>>>>>>>>>Add User <<<<<<<<<<<\e[0m"
 useradd roboshop
 
+echo -e "\e[36m>>>>>>>>>>Add a Directory <<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
+echo -e "\e[36m>>>>>>>>>> Unzip App Contents <<<<<<<<<<<\e[0m"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 
+echo -e "\e[36m>>>>>>>>>> Install NodeJS dependencies <<<<<<<<<<<\e[0m"
 npm install
 
-cp cart.service /etc/systemd/system/cart.service
+echo -e "\e[36m>>>>>>>>>> Copy Cart Systemd file <<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 
 systemctl daemon-reload
 
