db29bfc24e608892a7d0eb1c60d36aaeeab2822c
Girish <girishiva1989@gmail.com>
2023-06-10 18:25:51 +0530

    updated comments Add domain names

--- dispatch.sh
+++ dispatch.sh
@@ -1,19 +1,27 @@
+echo -e "\e[36m>>>>>>>>>> Install Golang <<<<<<<<<<<\e[0m"
 yum install golang -y
 
+echo -e "\e[36m>>>>>>>>>> IAdd User <<<<<<<<<<<\e[0m"
 useradd roboshop
 
+echo -e "\e[36m>>>>>>>>>> Create Application Directory <<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
+echo -e "\e[36m>>>>>>>>>> Unzip App Contents <<<<<<<<<<<\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
 unzip /tmp/dispatch.zip
 
+echo -e "\e[36m>>>>>>>>>> Install Golang dependencies <<<<<<<<<<<\e[0m"
 go mod init dispatch
 go get
 go build
 
-cp dispatch.service /etc/systemd/system/dispatch.service
+echo -e "\e[36m>>>>>>>>>> Copy Dispatch Systemd file <<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
 
+echo -e "\e[36m>>>>>>>>>> Start Dispatch Services <<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 
 systemctl enable dispatch
