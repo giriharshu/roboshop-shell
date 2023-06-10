5a0c4570ffb75451ec47393c3ca8e4d0e163ede3
Girish <girishiva1989@gmail.com>
2023-06-10 16:22:26 +0530

    updated comments

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,11 +1,16 @@
+echo -e "\e[36m>>>>>>>>>>Configure YUM Repos from the script provided by vendor <<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
+echo -e "\e[36m>>>>>>>>>>Configure YUM Repos for RabbitMQ <<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
+echo -e "\e[36m>>>>>>>>>>Install RabbitMQ <<<<<<<<<<<\e[0m"
 yum install rabbitmq-server -y
 
+echo -e "\e[36m>>>>>>>>>>Start RabbitMQ <<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
+echo -e "\e[36m>>>>>>>>>>create one user for the application <<<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
