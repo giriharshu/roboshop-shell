5c7e5dc09a814e7e1949d37640706047dc4a12f3
Girish <girishiva1989@gmail.com>
2023-06-17 14:11:24 +0530

    updated pip install

--- payment.sh
+++ payment.sh
@@ -16,8 +16,7 @@ cd /app
 unzip /tmp/payment.zip
 
 echo -e "\e[36m>>>>>>>>>> Install Python dependencies <<<<<<<<<<<\e[0m"
-#pip3.6 install -r requirements.txt
-pip3.6 install --user
+pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>>> Copy Payment Systemd file <<<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
