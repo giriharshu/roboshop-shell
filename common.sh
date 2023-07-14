c22943a27ceaf7004c1ba4283ca4e695510ac8ec
Girish <girishiva1989@gmail.com>
2023-07-14 10:29:04 +0530

    Added functions

--- common.sh
+++ common.sh
@@ -1,54 +1,91 @@
 app_user=roboshop
 
-print_head(){
+function_print_head(){
   echo -e "\e[35m>>>>>>>>>> $1 <<<<<<<<<<<\e[0m"
 }
 
-schema_setup(){
+function_schema_setup(){
   if [ "$schema_setup" == "mongo" ]
   then
-   print_head "Copy Mangodb repo"
+   function_print_head "Copy Mangodb repo"
    cp ${script_path}/mango.repo /etc/yum.repos.d/mongo.repo
 
-   print_head "Install Mangodb Client"
+   function_print_head "Install Mangodb Client"
    yum install mongodb-org-shell -y
 
-   print_head " Load Schema"
+   function_print_head " Load Schema"
 
    mongo --host mangodb-dev.gdevops89.online </app/schema/${component}.js
 
    systemctl restart ${component}
    fi
+
+   if [ "$schema_setup" == "mysql" ]
+   then
+     function_print_head "Load the Schema"
+       mysql -h mysql-dev.gdevops89.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+
+       function_print_head "Install Mysql"
+       yum install mysql -y
+   fi
 }
 
-function_nodejs() {
-  print_head "Configuring NodeJS repos"
+function_app_prereq()
+{
+  function_print_head "Add USer"
+    useradd roboshop
+
+    function_print_head "Create Application Directory"
+    rm -rf /app
+    mkdir /app
+
+    function_print_head "Unzip App Contents" <<<<<<<<<<<\e[0m""
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    cd /app
+    unzip /tmp/${component}.zip
+}
+
+function_systemd_setup()
+{
+    function_print_head "Copy Shipping Systemd file"
+    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+    function_print_head "Start Shipping Services"
+    systemctl daemon-reload
+    systemctl start ${component}
+    systemctl restart ${component}
+}
+function_nodejs()
+{
+  function_print_head "Configuring NodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print_head "Install NodeJS"
+  function_print_head "Install NodeJS"
   yum install nodejs -y
 
-  print_head "Add User"
-  useradd roboshop
+  function_app_prereq
 
-  print_head "Add a Directory"
-  rm -rf /app
-  mkdir /app
-
-  print_head "Unzip App Contents"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-  unzip /tmp/${component}.zip
-
-  print_head "Install NodeJS dependencies"
+  function_print_head "Install NodeJS dependencies"
   npm install
 
-  print_head "Copy ${component} Systemd file"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  function_schema_setup
 
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl start ${component}
+  function_systemd_setup
 
-  schema_setup
+}
+
+function_java()
+{
+  function_print_head "Install Maven"
+  yum install maven -y
+
+  function_app_prereq
+
+  function_print_head "Install Maven dependencies"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+  function_schema_setup
+
+  function_systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,4 @@
 script=$(realpath "$0")
-
-echo ${script}
-
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
@@ -12,40 +9,6 @@ then
   exit
 fi
 
-dirname $0
-
-echo -e "\e[36m>>>>>>>>>>Install Maven <<<<<<<<<<<\e[0m"
-yum install maven -y
-
-echo -e "\e[36m>>>>>>>>>>Add USer <<<<<<<<<<<\e[0m"
-useradd roboshop
-
-echo -e "\e[36m>>>>>>>>>>Create Application Directory <<<<<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>>>>> Unzip App Contents <<<<<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-
-echo -e "\e[36m>>>>>>>>>> Install Maven dependencies <<<<<<<<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[36m>>>>>>>>>> Copy Shipping Systemd file <<<<<<<<<<<\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>>>>>> Start Shipping Services <<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-
-systemctl enable shipping
-systemctl start shipping
-
-echo -e "\e[36m>>>>>>>>>> Install Mysql <<<<<<<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>>>>>> Load the Schema <<<<<<<<<<<\e[0m"
-mysql -h mysql-dev.gdevops89.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-systemctl restart shipping
+component=shipping
+schema_setup=mysql
+function_java
